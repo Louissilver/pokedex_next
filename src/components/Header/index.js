7b376a822f3456841495a75b361f1b1b780ee73a
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2021-02-16 11:26:04 -0300

    Ajustes em posicionamento do header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,67 +2,117 @@ import React from 'react';
 import styled from 'styled-components';
 
 const HeaderBase = styled.header`
-    padding: 0;
-    margin: 0;
-    width: 100%;
+    align-items: center;
     background-color: ${({ theme }) => theme.colors.secondary};
     display: flex;
+    flex-direction: column;
     justify-content: space-between;
-    align-items: center;
+    margin: 0;
+    padding: 1rem;
+    width: 100%;
+    @media screen and (min-width: 768px){
+        flex-direction: row;
+        padding: .25rem;
+    }
 `;
 
 HeaderBase.Search = styled.div`
-    margin: 0;
-    padding: 0;
-    width:25%;
-    display: flex;
-    justify-content: center;
     align-items: center;
+    display: flex;
     font-size: 1rem;
+    justify-content: center;
+    margin: 0;
+    order: 3;
+    padding: 0;
+    width: 100%;
+    @media screen and (min-width: 768px){
+        width: 25%;
+    }
 `;
 
 HeaderBase.Input = styled.input`
-    padding: 8px;
     border: 2px solid ${({ theme }) => theme.colors.primary};
+    font-size: 1.5rem;
+    padding: .5rem;
+    width: 75%;
+    &:focus{
+        border: 2px solid ${({ theme }) => theme.colors.primaryDark};
+        outline: 2px solid ${({ theme }) => theme.colors.secondaryDark};
+    }
+    @media screen and (min-width: 768px){
+        font-size: 1rem;
+    }
 `;
 
 HeaderBase.Button = styled.button`
-    padding: 8px;
     border: 2px solid ${({ theme }) => theme.colors.primary};
     background-color: ${({ theme }) => theme.colors.primary};
     color: ${({ theme }) => theme.colors.secondary};
+    font-size: 1.5rem;
+    padding: .5rem;
+    width: 25%;
     &:hover{
         cursor: pointer;
         background-color: ${({ theme }) => theme.colors.primaryDark};
         border: 2px solid ${({ theme }) => theme.colors.primaryDark};
     }
+    @media screen and (min-width: 768px){
+        font-size: 1rem;
+    }
 `;
 
 HeaderBase.Logo = styled.a`
+    order: 1;
     &:hover{
         cursor: pointer;
+    }
+    @media screen and (min-width: 768px){
+        flex-direction: row;
+        justify-content: space-between;
+        margin: 0;
+        order: 2;
     }
 `;
 
 HeaderBase.Image = styled.img`
-   width: 8rem; 
+   width: 16rem; 
+   @media screen and (min-width: 768px){
+    width: 8rem;     
+    }
 `;
 
 HeaderBase.Nav = styled.nav`
-    width:25%;
     padding: 0;
+    margin: 1.5rem;
     display: flex;
+    flex-direction: column;
+    order: 2;
+    width:100%;
+    @media screen and (min-width: 768px){
+        flex-direction: row;
+        margin: 0;
+        order: 1;
+        width:25%;
+    }
 `;
 
 HeaderBase.Link = styled.a`
     color: ${({ theme }) => theme.colors.primary};
+    display: inline-block;
     font-family: sans-serif;
-    font-weight: bold;
-    border-style: none;
-    display: block;
-    padding: 1rem;
+    font-size: 1.5rem;
+    padding: 1em;
+    text-align: center;
+    text-decoration: none;
+    width: 100%;
     &:hover{
         background-color: ${({ theme }) => theme.colors.secondaryDark};
+    }
+    @media screen and (min-width: 768px){
+        flex-direction: row;
+        font-size: 1rem;
+        justify-content: space-between;
+        width: auto;
     }
 `;
 
@@ -84,10 +134,10 @@ export default function Header() {
                 <HeaderBase.Image src='https://fontmeme.com/permalink/210216/84f1943cbd05e36bb90d12663346a34b.png' />
             </HeaderBase.Logo>
             <HeaderBase.Search>
-            <HeaderBase.Input type="text" placeholder="Search..." />
-            <HeaderBase.Button>
-                Pesquisar
-            </HeaderBase.Button>
+                <HeaderBase.Input type="text" placeholder="Search..." />
+                <HeaderBase.Button>
+                    X
+                </HeaderBase.Button>
             </HeaderBase.Search>
         </HeaderBase>
     )
