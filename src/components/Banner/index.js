fa67eb65358c84bfdc108b152f2efb5072e31250
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2021-02-19 21:57:52 -0300

    ajuste no banner

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -3,8 +3,11 @@ import styled from 'styled-components'
 
 const Banner = styled.img`
     width: 100%;
+    height: 100%;
     margin: 0;
     padding: 0;
+    object-fit: cover;
+    object-position: center;
 `;
 
 export default Banner;
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -3,12 +3,12 @@ import styled from 'styled-components';
 
 const MainBase = styled.main`
     flex: 1;
-    padding-left: 2%;
-    padding-right: 2%;
     background-repeat: no-repeat;
     background-size: cover;
     width: 100%;
     height: 100%;
+    margin: 0;
+    padding: 0;
 `;
 
 export default function Main({ children }) {
