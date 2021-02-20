fd4edef3b31bd10665589161b44a99f5ada0deb7
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2021-02-19 21:38:50 -0300

    incluindo banner na home

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import PageDefault from '../src/components/PageDefault'
+import Banner from '../src/components/Banner'
 
 export default function Home() {
   return (
     <>
       <PageDefault>
-        Home
+        <Banner src="https://img.ibxk.com.br///2019/09/30/30091641838086.jpg?w=1200&h=675&mode=crop&scale=both"/>
       </PageDefault>
     </>
   )
---
+++ src/components/Banner/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components'
+
+const Banner = styled.img`
+    width: 100%;
+    margin: 0;
+    padding: 0;
+`;
+
+export default Banner;
